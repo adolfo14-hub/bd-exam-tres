d241ac26e469b6c0fe972713c96d9411fa34765c
adolfo14-hub <80058049+adolfo14-hub@users.noreply.github.com>
2021-11-25 18:38:19 -0600

    Update select.php

--- select.php
+++ select.php
@@ -10,7 +10,7 @@ try {
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $sql = 'SELECT * FROM Cancione';
+   $sql = 'SELECT id, nombre, artista, fecha, productora FROM Cancione';
 
     foreach ($conn->query($sql) as $row) {
         
