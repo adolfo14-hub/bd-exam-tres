d961ca6a9e7db94ebe10272bb0dec99b44af6c3d
adolfo14-hub <80058049+adolfo14-hub@users.noreply.github.com>
2021-11-25 18:34:44 -0600

    Update insert.php

--- insert.php
+++ insert.php
@@ -10,11 +10,11 @@ try {
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $sql .= "INSERT INTO Canciones (nombre, artista, fecha, productora) VALUES ('Hanging Tree', 'Michael Bibi', '2015','Sony Music')";
-    $sql .= "INSERT INTO Canciones (nombre, artista, fecha, productora) VALUES ('Get Stupid', 'Cloone', '2018','CLNE')";
-    $sql .= "INSERT INTO Canciones (nombre, artista, fecha, productora) VALUES ('Gravity', 'Boris Brejcha', '2020','Warner Music')";
-    $sql .= "INSERT INTO Canciones (nombre, artista, fecha, productora) VALUES ('Marea', 'Fred Again', '2021','Again Records')";
-    $sql .= "INSERT INTO Canciones (nombre, artista, fecha, productora) VALUES ('Dance and Chant', 'Yolanda BeCool', '2019','LLC Records')";
+    $sql .= "INSERT INTO Cancione (nombre, artista, fecha, productora) VALUES ('Hanging Tree', 'Michael Bibi', '2015','Sony Music')";
+    $sql .= "INSERT INTO Cancione (nombre, artista, fecha, productora) VALUES ('Get Stupid', 'Cloone', '2018','CLNE')";
+    $sql .= "INSERT INTO Cancione (nombre, artista, fecha, productora) VALUES ('Gravity', 'Boris Brejcha', '2020','Warner Music')";
+    $sql .= "INSERT INTO Cancione (nombre, artista, fecha, productora) VALUES ('Marea', 'Fred Again', '2021','Again Records')";
+    $sql .= "INSERT INTO Cancione (nombre, artista, fecha, productora) VALUES ('Dance and Chant', 'Yolanda BeCool', '2019','LLC Records')";
     
     // use exec() because no results are returned
     $conn->exec($sql);
